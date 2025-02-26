52021be5766e7ce14786a60cbc42b3dfcc32a1e5
ibrohim0725 <baxtiyorovibrohim513@gmail.com>
2025-02-26 20:12:06 +0500

    Create any methods

--- script.js
+++ script.js
@@ -1,28 +1,50 @@
-
-
-// // // Split metodi
-// Split metodi matinni berilgan ajratuvchi bilan ajratadi hamda uni massivga aylantiradi
-Array.prototype.split2 = function (el) {};
-
-const r = (arg1, el) => {
-  let a = [];
-  let b = "";
-  for (let i = 0; i < el.length; i++) {
-    if (el[i] == arg1) {
-      b && a.push(b);
-      b = "";
+// Capitalize metodi
+let srt = "salom  dunyo";
+String.prototype.myCapitalize = function () {
+  let res = "";
+  let capitalize = true;
+  for (let el of this) {
+    if (capitalize && el != " ") {
+      res += el.toUpperCase();
+      capitalize = false;
     } else {
-      b = b + el[i];
+      res += el;
+    }
+    if (el === " ") {
+      capitalize = true;
     }
   }
-  b && a.push(b);
-  return a;
+  console.log(res);
 };
+srt.myCapitalize(() => {});
+// // // Split metodi
+// Split metodi matinni berilgan ajratuvchi bilan ajratadi hamda uni massivga aylantiradi
+const str1 = "hello";
+String.prototype.mySplit = function () {
+  console.log(this);
+};
+str1.mySplit(() => {});
 
-console.log(r("l", "salom hello"));
+// Sort metodi
+Array.prototype.mySort = function () {
+  let n = this.length;
 
-// ForEach metodi
-// ForEach metodi bu berilan arrayni bir marta to'liq aylanib chiqadi
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - 1; j++) {
+      if (this[j] > this[j + 1]) {
+        let temp = this[j];
+        this[j] = this[j + 1];
+        this[j + 1] = temp;
+      }
+    }
+  }
+  return this;
+};
+let arr5 = [15, 68, 95, 26, 3, 15, 478];
+console.log(arr5.mySort());
+
+// // ForEach metodi
+// // ForEach metodi bu berilan arrayni bir marta to'liq aylanib chiqadi
 Array.prototype.myForEach = function (callback) {
   for (let i = 0; i < this.length; i++) {
     callback(this[i], i, this);
@@ -31,7 +53,7 @@ Array.prototype.myForEach = function (callback) {
 const num = [10, 20, 30, 40, 50];
 
 num.myForEach((son) => {
-  console.log("Element" + son);
+  console.log("Element" + " " + son);
 });
 
 // Map metodi
@@ -62,9 +84,87 @@ Array.prototype.myFilter = function (callback) {
   return res;
 };
 
-let arr3=[10,20,30,40,50];
+let arr3 = [10, 20, 30, 40, 50];
 
-let arr4=arr3.myFilter((value)=>{
-  return value %2==0;
-})
+let arr4 = arr3.myFilter((value) => {
+  return value % 2 == 0;
+});
 console.log(arr4);
+
+// Join metodi
+Array.prototype.myJoin = function (callback) {
+  let arrStrjoin = "";
+  for (let i = 0; i < this.length; i++) {
+    if (i === this.length - 1) {
+      arrStrjoin += this[i];
+    } else {
+      arrStrjoin += this[i] + callback;
+    }
+  }
+  return arrStrjoin;
+};
+let arr = [10, 20, 30, 40, 50];
+console.log(arr.myJoin(" "));
+
+// PadEnd metodi
+String.prototype.myPadEnd = function (callback, padstring = "") {
+  if (this.length >= callback) {
+    return this.toString();
+  }
+  let res = this.toString();
+  while (res.length < callback) {
+    res += padstring;
+  }
+
+  return res;
+};
+let strpadend = "Salom";
+console.log(strpadend.myPadEnd(20, "*"));
+
+// padStart metodi
+String.prototype.myPadsatrt = function (callback, padstring = "") {
+  if (this.length >= callback) {
+    return this.toString();
+  }
+  let res = this.toString();
+  while (res.length < callback) {
+    res = padstring + res;
+  }
+  return res;
+};
+let strPadstrat = "Salom";
+console.log(strPadstrat.myPadsatrt(10, "*"));
+
+// Includes metodi
+String.prototype.myIncludes = function (searchstr, position = 0) {
+  if (position < 0) {
+    position = 0;
+  }
+  let searchlength = searchstr.length;
+
+  if (searchlength === 0) {
+    return true;
+  }
+
+  for (let i = position; i <= this.length - searchlength; i++) {
+    if (this.slice(i, i + searchlength) === searchstr) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+let str3 = "Salom";
+console.log(str3.myIncludes("d"));
+
+// Length
+String.prototype.myLength = function () {
+  let counter = 0;
+  for (let i = 0; i < this.length; i++) {
+    counter++;
+  }
+  console.log(counter);
+};
+let str2 = "Hello ";
+str2.myLength();
